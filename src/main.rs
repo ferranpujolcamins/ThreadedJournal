3be41592169b5c1734117282266240978301e3d1
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2023-01-21 17:06:22 +0100

    Get something with Dioxus and Fermi

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,62 @@
 use dioxus::prelude::*;
-use dioxus_desktop::*;
+use fermi::prelude::*;
 
+#[derive(PartialEq)]
+struct Item {
+    text: String,
+}
 
-fn main() {
-    dioxus_desktop::launch(app);
+#[derive(PartialEq)]
+struct Entry {
+    items: Vec<Item>,
+}
+
+// TODO: AtomFamily instead?
+static ENTRIES: Atom<Vec<Entry>> = |_| {
+    vec![
+        Entry {
+            items: vec![Item {
+                text: "I did something".to_string(),
+            }],
+        },
+        Entry {
+            items: vec![Item {
+                text: "Also today".to_string(),
+            }],
+        },
+    ]
+};
+
+#[inline_props]
+fn item_element<'a>(cx: Scope, item: &'a Item) -> Element {
+    cx.render(rsx! {
+        div {
+            div { "{item.text}"}
+        }
+    })
+}
+
+#[inline_props]
+fn entry_element<'a>(cx: Scope, entry: &'a Entry) -> Element {
+    cx.render(rsx! {
+        div {
+            entry.items.iter().map(|item| rsx!(
+                div {
+                    item_element { item: item }
+                }
+            ))
+        }
+    })
 }
 
 fn app(cx: Scope) -> Element {
-    cx.render(rsx! (
-        div { "Hello, world!" }
-    ))
+    use_init_atom_root(cx);
+    let entries = use_read(cx.scope, ENTRIES);
+    cx.render(rsx!(entries
+        .iter()
+        .map(|entry| rsx!(entry_element { entry: entry }))))
+}
+
+fn main() {
+    dioxus_desktop::launch(app);
 }
